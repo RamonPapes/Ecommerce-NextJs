7a1a58b208b21fd26ecde71ebda08f43e1c823c8
RamonPapes <ramonpapes@gmail.com>
2024-04-11 15:35:38 -0300

    feat: Add modal in index page | Fix: Add responsive design in products cards and carousel banner | fix: Prices and Stars rounded to bottom

--- src/components/produto.tsx
+++ src/components/produto.tsx
@@ -11,8 +11,9 @@ interface ProdutoProps {
 const Produto: React.FC<ProdutoProps> = ({ image, title, price, rating, onClick }) => {
     const renderRatingStars = () => {
         const stars = [];
+        const roundedRating = Math.floor(rating);
         for (let i = 0; i < 5; i++) {
-            if (i < rating) {
+            if (i < roundedRating) {
                 stars.push(
                     <svg key={i} className="w-4 h-4 text-yellow-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
                         <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
@@ -40,12 +41,12 @@ const Produto: React.FC<ProdutoProps> = ({ image, title, price, rating, onClick 
                     <div className="flex items-center space-x-1 rtl:space-x-reverse">
                         {renderRatingStars()}
                     </div>
-                    <span className="bg-blue-100 text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded">{rating}</span>
+                    <span className="bg-blue-100 text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded">{rating.toFixed(2)}</span>
                 </div>
-                <div className="flex items-center justify-between">
-                    <span className="text-3xl font-bold text-gray-900">${price}</span>
-                    <button className="text-white bg-blue-500 hover:bg-blue-600 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center" onClick={onClick}>
-                        Saiba mais
+                <span className="text-3xl font-bold text-gray-900">${price.toFixed(2)}</span>
+                <div className="mt-4 flex items-end justify-center">
+                    <button className="text-white bg-blue-900 hover:bg-blue-600 transition-colors font-medium rounded-lg text-sm px-5 py-2.5 text-center" onClick={onClick}>
+                        know more
                     </button>
                 </div>
             </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,16 @@
 import React, { useState, useEffect } from 'react';
+import ReactModal from 'react-modal';
 import Produto from '@/components/produto';
 import Carousel from '@/components/carrossel';
+import { Icon } from '@iconify/react/dist/iconify.js';
+
 
 const Main = () => {
   const slides = ['banner1.png', 'banner2.png', 'banner3.png'];
   const [categorias, setCategorias] = useState<string[]>([]);
   const [produtosPorCategoria, setProdutosPorCategoria] = useState<{ [key: string]: Produto[] }>({});
-  const [categoriaIndices, setCategoriaIndices] = useState<{ [key: string]: number }>({});
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [produtoSelecionado, setProdutoSelecionado] = useState<Produto | null>(null);
 
   useEffect(() => {
     const fetchProdutos = async () => {
@@ -29,30 +33,28 @@ const Main = () => {
     fetchProdutos();
   }, []);
 
+  const closeModal = () => {
+    setModalIsOpen(false);
+    setProdutoSelecionado(null);
+  };
+
   return (
     <main className="min-w-full">
       <div className="flex justify-center w-full flex-col items-center">
-        <div className="w-[80%]">
+
+        {/* Banner hidden a partir de 960px */}
+        <div className="w-[80%] max-sm:hidden">
           <Carousel slides={slides}></Carousel>
         </div>
         <div className="w-[80%]">
           {categorias.map((categoria) => {
-            const currentIndex = categoriaIndices[categoria] || 0;
             const categoriaProdutos = produtosPorCategoria[categoria] || [];
             return (
               <div key={categoria}>
-                <div className='w-full flex justify-start items-center'>
-                  <h2 className='font-semibold'>{categoria}</h2>
-                  <div className='flex items-center justify-center gap-3'>
-                    {/* <button onClick={() => previewProduct(categoria)}>
-                      <Icon icon={"fa:arrow-left"}></Icon>
-                    </button>
-                    <button onClick={() => nextProduct(categoria)}>
-                      <Icon icon={"fa:arrow-right"}></Icon>
-                    </button> */}
-                  </div>
+                <div className='flex justify-start items-center my-4'>
+                  <h1 className='font-semibold text-xl'>{categoria}</h1>
                 </div>
-                <div className='flex overflow-hidden flex-wrap justify-between gap-y-16 my-6'>
+                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 max-sm:flex max-sm:justify-center max-sm:items-center max-sm:flex-wrap">
                   {categoriaProdutos.map((produto, index) => (
                     <Produto
                       key={index}
@@ -60,7 +62,10 @@ const Main = () => {
                       title={produto.title}
                       price={produto.price}
                       rating={produto.rating.rate}
-                      onClick={() => { }}
+                      onClick={() => {
+                        setProdutoSelecionado(produto);
+                        setModalIsOpen(true);
+                      }}
                     />
                   ))}
                 </div>
@@ -68,8 +73,51 @@ const Main = () => {
             );
           })}
         </div>
+        <ReactModal
+          isOpen={modalIsOpen}
+          onRequestClose={closeModal}
+          className="fixed inset-0 flex items-center justify-center"
+          overlayClassName="fixed inset-0 bg-gray-900 bg-opacity-50"
+          contentLabel="Product Details"
+        >
+          <div className="flex flex-col justify-center h-screen">
+            <div className="relative flex flex-col md:flex-row md:space-x-5 space-y-3 md:space-y-0 rounded-xl shadow-lg p-3 max-w-xs md:max-w-3xl mx-auto border border-white bg-white">
+              <div className="w-full md:w-1/3 bg-white grid place-items-center">
+                {produtoSelecionado && (
+                  <img
+                    src={produtoSelecionado.image}
+                    alt={produtoSelecionado.title}
+                    className="w-40 md:w-auto"
+                  />
+                )}
+              </div>
+              <div className="w-full md:w-2/3 bg-white flex flex-col space-y-2 p-3">
+                <div className="flex justify-between item-center">
+                  <p className="text-gray-500 font-medium hidden md:block">
+                    {produtoSelecionado && produtoSelecionado.category}
+                  </p>
+                  <button
+                    className="text-gray-500 hover:text-gray-700"
+                    onClick={closeModal}
+                  >
+                    <Icon icon="majesticons:close-line" className="w-6 h-6" />
+                  </button>
+                </div>
+                <h3 className="font-black text-gray-800 md:text-3xl text-xl">
+                  {produtoSelecionado && produtoSelecionado.title}
+                </h3>
+                <p className="md:text-lg text-gray-500 text-base">
+                  {produtoSelecionado && produtoSelecionado.description}
+                </p>
+                <p className="text-xl font-black text-gray-800">
+                  {produtoSelecionado && produtoSelecionado.price.toFixed(2)} R$
+                </p>
+              </div>
+            </div>
+          </div>
+        </ReactModal>
       </div>
-    </main>
+    </main >
   );
 };
 
